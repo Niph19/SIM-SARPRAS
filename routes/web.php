6832237b63dd3ccc9f0c045b0f257c061f7fe950
Niph19 <130121624+Niph19@users.noreply.github.com>
2026-09-08 13:56:21 +0700

    auth role done

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('/laporan', LaporanController::class)->middleware(['auth', 'role:murid']);
+
+Route::middleware('auth', 'role:admin')->group(function () {
+    Route::resource('/dashboardAdmin', DashboardAdminController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
